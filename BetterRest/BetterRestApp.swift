db72d89ce1efbf1357fa91e4e6ea2c98223be796
MeryemEroglu <200101022@ogrenci.yalova.edu.tr>
2025-08-26 23:37:40 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Meryem Eroğlu on 26.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
