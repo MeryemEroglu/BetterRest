ef3dfd9e2df8dbe0517eaedacb0578efcfa0a039
MeryemEroglu <200101022@ogrenci.yalova.edu.tr>
2025-09-03 02:48:38 +0300

    Added real-time bedtime calculation and redesigned UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,10 +13,6 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
     
-    @State private var alerTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 8
@@ -24,39 +20,7 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date.now
     }
     
-    var body: some View {
-        NavigationStack {
-            Form {
-                VStack (alignment: .leading, spacing: 5) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                }
-                VStack (alignment: .leading, spacing: 5) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12,
-                            step: 0.25)
-                    
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
-                }
-            }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
-            }
-            .alert(alerTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
-        }
-        
-    }
-    func calculateBedTime() {
+    var recommendedSleepTime: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -65,17 +29,44 @@ struct ContentView: View {
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount,
-                                                  coffee: Double(coffeeAmount))
-            let sleepTime = wakeUp - prediction.actualSleep
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
-            alerTitle = "Your ideal sleep time is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            let sleepTime = wakeUp - prediction.actualSleep
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+            
         } catch {
-            alerTitle = "Error"
-            alertMessage = "Sorry, an error occurred while calculating your sleep time."
+            return "Sorry, there was a problem calculating your sleep time"
         }
-        showingAlert = true
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Your recommend sleep time is..."){
+                    Text(recommendedSleepTime)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.indigo)
+                }
+                Section("When do you want to wake up?") {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                Section("Desired amount of sleep") {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12,
+                            step: 0.25)
+                }
+                Section("Daily coffee intake") {
+                    Picker("^[\(coffeeAmount) cup](inflect: true)", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text("\($0) \($0 == 1 ? "cup" : "cups")")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("BetterRest")
+            
+        }
     }
 }
 
